64c4c8fa708b9ded5294217bfe0cc75b4b1df1ef
Italo Castro <102182915+devcastroitalo@users.noreply.github.com>
2022-12-18 06:02:33 -0300

    Update init.el

--- init.el
+++ init.el
@@ -35,11 +35,13 @@
 (add-to-list 'default-frame-alist '(height . 50))
 (add-to-list 'default-frame-alist '(width . 150))
 
-(require 'package)
-(setq package-enable-at-startup nil)
+(require 'ido)
+(ido-mode t)
+
+(load-theme 'gruber-darker t)
 
 (add-to-list 'package-archives
-			 '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
@@ -57,13 +59,14 @@
 	(ac-config-default)
 	(global-auto-complete-mode t)))
 
-;; MELPA stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(custom-safe-themes
+   '("7923541211298e4fd1db76c388b1d2cb10f6a5c853c3da9b9c46a02b7f78c882" default))
+ '(package-selected-packages '(gruber-darker-theme use-package try auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
